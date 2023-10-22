3aeeef486378e1acf33b7c764e4f4bebb1c20bd2
Oskar Wiszowaty <ppwiszu@gmail.com>
2023-10-22 23:34:03 +0200

    Adding lazygit mapping

--- lua/wishu/plugins/lazygit.lua
+++ lua/wishu/plugins/lazygit.lua
@@ -1,6 +1,10 @@
 return{
 	"kdheepak/lazygit.nvim",
+  action = "LazyGit",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	config = function()
+		vim.keymap.set("n", "<leader>gg", vim.cmd.LazyGit)
+  end
 }
